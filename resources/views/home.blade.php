23fceb4f80bb6254db2781c0bf1016dc2f86b163
KylosovSergeyAlexandrovich <formozaiti@gmail.com>
2020-05-03 16:37:03 +0300

    start + role

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Главная</div>
+                <p class="alert">Вы <?php echo e(Auth::user()->role); ?></p>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    Вы успешно авторизованы!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
